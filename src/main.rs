f4a5e3253fef0b45476ed435508c39d581e04caf
Super User <aresrebatt@gmail.com>
2025-03-20 16:51:41 +0100

    Inserimento verifica comandi non funzionante

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,37 @@
-use pcap::Device;
-
+//use pcap::Device;
+use std::io;
+use std::io::Write;
 fn main() {
-    let mut cap = Device::lookup().unwrap().unwrap().open().unwrap();
+    println!("Configure firewall");
+    loop{
+    	
+		print!(">> ");
+		
+		let mut input = String::new();
+    	io::stdout().flush().unwrap();
+    	io::stdin()
+    		.read_line(&mut input)
+    		.expect("Error");
 
-    while let Ok(packet) = cap.next_packet() {
-        println!("{:?}", packet);
+    	match execute_command(&input){
+    		Ok(_)=> println!("The rule was sucessfully inserted"),
+    		Err(e) => println!("Error: {}", e)
+    	}
     }
+}
+
+fn execute_command(_command: &str)-> Result<i32, &str>{
+
+	
+	let command_param: Vec<&str> = _command.split_whitespace().collect();
+	
+	match command_param[0]{
+		"ip-address"=> println!("Comando valido"),
+		"port" => println!("Comando valido"),
+		"sh" => println!("Comando valido"),
+		"rm-rule" => println!("Comando valido"),
+		_ => return Err("Unrecognized command")
+	}
+
+	return Ok(0);
 }
